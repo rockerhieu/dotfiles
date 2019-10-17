855c05a5051dcf1480cf6f72895c7fafbff8d266
Hieu <hieu@essential.com>
2019-10-16 23:54:27 -0700

    updates flutter.sh to work for both Ubuntu and Mac

--- flutter.sh
+++ flutter.sh
@@ -1,5 +1,5 @@
 wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.2-stable.zip
-tar -xf flutter_macos_v1.9.1+hotfix.2-stable.zip
+unzip -q flutter_macos_v1.9.1+hotfix.2-stable.zip
 rm flutter_macos_v1.9.1+hotfix.2-stable.zip
 mkdir -p ~/Development/libraries
 mv flutter ~/Development/libraries/
