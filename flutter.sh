4f764afb163d3e0a0f231690a1f95eafc2cd0f02
admin <admin@admins-mbp.lan>
2020-05-09 22:19:11 -0700

    Updates flutter

--- flutter.sh
+++ flutter.sh
@@ -1,6 +1,6 @@
-wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.2-stable.zip
-unzip -q flutter_macos_v1.9.1+hotfix.2-stable.zip
-rm flutter_macos_v1.9.1+hotfix.2-stable.zip
+wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.17.0-stable.zip
+unzip -q flutter_macos_1.17.0-stable.zip
+rm flutter_macos_1.17.0-stable.zip
 mkdir -p ~/Development/libraries
 mv flutter ~/Development/libraries/
-ln -s ~/Development/libraries/flutter /usr/local/share/flutter
+ln -s ~/Development/libraries/flutter/bin/flutter /usr/local/bin/flutter
