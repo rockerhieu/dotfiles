4bc706f75466ee12079ac085cdc6862a2084e49f
Hieu Hua <rockerhieu@gmail.com>
2020-05-26 19:34:32 -0700

    Accepts all Android SDK licenses when installing flutter

--- flutter.sh
+++ flutter.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.17.0-stable.zip --output flutter_macos_1.17.0-stable.zip #--silent
 #wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.17.0-stable.zip
 unzip -q flutter_macos_1.17.0-stable.zip
@@ -5,3 +7,6 @@ rm flutter_macos_1.17.0-stable.zip
 mkdir -p ~/Development/libraries
 mv flutter ~/Development/libraries/
 ln -s ~/Development/libraries/flutter/bin/flutter /usr/local/bin/flutter
+
+# accept all android licences
+yes | flutter doctor --android-licenses
