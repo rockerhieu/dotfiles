1f29a320c608adca43910c9e1b30193897955fd1
Hieu Hua <rockerhieu@gmail.com>
2020-05-26 19:35:06 -0700

    Specifies bash shell when executing zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
