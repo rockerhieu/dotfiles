3877af26c3f9d7b3a9174ecd224ee963f7b01628
Hieu Hua <hieu@Hieus-MacBook-Pro.local>
2021-09-11 23:26:28 -0500

    feat: use antigen to simplify .zshrc

--- brew.sh
+++ brew.sh
@@ -56,32 +56,6 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-#brew install aircrack-ng
-#brew install bfg
-#brew install binutils
-#brew install binwalk
-#brew install cifer
-#brew install dex2jar
-#brew install dns2tcp
-#brew install fcrackzip
-#brew install foremost
-#brew install hashpump
-#brew install hydra
-#brew install john
-#brew install knock
-#brew install netpbm
-#brew install nmap
-#brew install pngcheck
-#brew install socat
-#brew install sqlmap
-#brew install tcpflow
-#brew install tcpreplay
-#brew install tcptrace
-#brew install ucspi-tcp # `tcpserver` etc.
-#brew install xpdf
-#brew install xz
-
 # Node
 brew install node
 
@@ -171,3 +145,6 @@ brew install pidcat
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+[highlight, apktool, pidcat, tmux]
+[openkey, caffeine, musescore, vysor, vlc, spotify, iterm2, google-chrome, spectacle, slack, visual-studio-code, appcleaner, transmission]
